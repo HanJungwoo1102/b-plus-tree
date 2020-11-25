7271c948eb8a8b15eb61094d56134a834ac4aad9
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2020-11-25 21:30:11 +0900

    feat:구현체 뼈대 만들기

--- BTree.cpp
+++ BTree.cpp
@@ -1,3 +1,58 @@
 // Please implement most of your source codes here. 
+#include "BTree.h"
 
+BTreeNode::BTreeNode() {
 
+}
+
+BTreeNode::~BTreeNode() {
+
+}
+
+NodeType BTreeNode::getNodeType() {
+
+}
+
+BTreeInternalNode::BTreeInternalNode() {
+
+}
+
+BTreeInternalNode::~BTreeInternalNode() {
+
+}
+
+BTreeLeafNode::BTreeLeafNode() {
+
+}
+
+BTreeLeafNode::~BTreeLeafNode() {
+
+}
+
+void BTreeLeafNode::printLeafNode() {
+
+}
+
+BTree::BTree() {
+
+}
+
+BTree::~BTree() {
+
+}
+
+void BTree::insert(long long value) {
+
+}
+
+void BTree::printLeafNode(long long value) {
+
+}
+
+void BTree::pointQuery(long long value) {
+
+}
+
+void BTree::rangeQuery(long long low, long long high) {
+
+}
